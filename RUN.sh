0a025d8bfcf5131611f3deb007538be02ff43697
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-05-31 22:25:35 -0700

    chore: Add container stop logic to RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -24,15 +24,6 @@ fi
 echo "Architecture: $1"
 echo "Version: $2"
 
-container = rusicsvelte:"$2"
-RUNNING_CONTAINERS=$(docker ps -af status=running --format 'image={{.Image}}')
-if echo "$RUNNING_CONTAINERS" | grep -q "$container"; then
-    echo "A container with the image $container\n is already running STOPPING IT NOW"
-    # stop the container with the image name $container
-    docker stop $(docker ps -q --filter ancestor=$container)
-
-fi
-
 if [ "$1" = "32" ]; then
     if [ "$(uname -m)" = "aarch64" ]; then
         echo "ERROR: This is a 64-bit ARM system."
@@ -44,6 +35,20 @@ if [ "$1" = "64" ]; then
     if [ "$(uname -m)" = "armv7l" ]; then
         echo "ERROR: This is a 32-bit ARM system."
         exit 1
+    fi
+fi
+
+# Stop the running rusic container if it exists
+container=rusicsvelte:"$2"
+echo container=$container
+if [ "$container" != "" ]; then
+    RUNNING_CONTAINERS=$(docker ps -af status=running --format 'image={{.Image}}')
+    if echo "$RUNNING_CONTAINERS" | grep -q "$container"; then
+        echo "A container with the name $container\n is already running STOPPING IT NOW"
+        # stop the container with the image name $container
+        docker stop $(docker ps -q --filter ancestor=$container)
+    else
+        echo "No container with the name $container is running"
     fi
 fi
 
