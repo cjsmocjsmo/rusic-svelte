4b7b50aca622903e96e0b3cd46910aed77cc680f
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-08-25 06:55:43 -0700

    chore: Update Dockerfile and RUN.sh to expose port 80 instead of 9090

--- rusicsvelte.py
+++ rusicsvelte.py
@@ -4,8 +4,24 @@ import subprocess
 
 CWD = os.getcwd()
 
-def install(version, docker_file, arch):
+def rusicsvelte_install(version, docker_file, arch):
     subprocess.run(['npm', 'install'])
+    subprocess.run(['npm', 'run', 'build'])
+    subprocess.run(['docker', 'build', '-t', f'rusicsvelte{arch}:{version}', '-f', docker_file, '.'])
+    subprocess.run(['docker', 'run', '-d', '-p', '9090:80', f'rusicsvelte{arch}:{version}'])
+
+def rusicsvelte_update(version, arch):
+    subprocess.run(['docker', 'stop', f'rusicsvelte{arch}'])
+    subprocess.run(['docker', 'rm', f'rusicsvelte{arch}'])
+    subprocess.run(['git', 'pull'])
+    subprocess.run(['npm', 'run', 'build'])
+    subprocess.run(['docker', 'build', '-t', f'rusicsvelte{arch}:{version}', '-f', f'Dockerfile{arch}', '.'])
+    subprocess.run(['docker', 'run', '-d', '-p', '9090:80', f'rusicsvelte{arch}:{version}'])
+
+def rusicsvelte_uninstall(version, arch):
+    subprocess.run(['docker', 'stop', f'rusicsvelte{arch}'])
+    subprocess.run(['docker', 'rm', f'rusicsvelte{arch}'])
+    # subprocess.run(['docker', 'rmi', f'rusicsvelte{arch}:{version}'])
 
 
 def main():
@@ -17,42 +33,32 @@ def main():
 
     args = parser.parse_args()
 
-    
     docker_32_file = os.path.join(CWD, "RPI", "32", "Dockerfile")
     docker_64_file = os.path.join(CWD, "RPI", "64", "Dockerfile")
-    print(docker_32_file)
-    print(docker_64_file)
+
+    arch = None
 
     if os.uname().machine == "armv7l":
         subprocess.run(["cp", "-pvr", docker_32_file, CWD])
         arch = "32"
-        if args.install:
-            print(f"Installing Rusic:{args.version}")
-            rusic_install(args.version, docker_32_file, arch)
-        elif args.update:
-            print(f"Updating Rusic:{args.version}")
-            rusic_update(args.version, docker_32_file, arch)
-        elif args.delete:
-            print(f"Deleting Rusic:{args.version}")
-            rusic_delete(args.version, arch)
-        else:
-            print("No action specified. Use -i, -u, or -d.")
     elif os.uname().machine == "aarch64":
         subprocess.run(["cp", "-pvr", docker_64_file, CWD])
         arch = "64"
-        if args.install:
-            print(f"Installing Rusic:{args.version}")
-            rusic_install(args.version, docker_64_file, arch)
-        elif args.update:
-            print(f"Updating Rusic:{args.version}")
-            rusic_update(args.version, docker_64_file, arch)
-        elif args.delete:
-            print(f"Deleting Rusic:{args.version}")
-            rusic_delete(args.version, docker_64_file, arch)
-        else:
-            print("No action specified. Use -i, -u, or -d.")
     else:
         print("Invalid architecture. Use 32 or 64.")
+
+    if args.install:
+        print(f"Installing Rusic:{args.version}")
+        rusicsvelte_install(args.version, docker_64_file, arch)
+    elif args.update:
+        print(f"Updating Rusic:{args.version}")
+        rusicsvelte_update(args.version, docker_64_file, arch)
+    elif args.delete:
+        print(f"Deleting Rusic:{args.version}")
+        rusicsvelte_uninstall(args.version, docker_64_file, arch)
+    else:
+        print("No action specified. Use -i, -u, or -d.")
+    
 
 if __name__ == "__main__":
     main()
